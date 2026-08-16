866e2e278da5a01bf489ded260f1d511536888de
palak <palak.solutionworld@gmail.com>
2026-08-16 16:29:10 +0530

    Update Background3D.tsx

--- src/Background3D.tsx
+++ src/Background3D.tsx
@@ -30,6 +30,7 @@ function Particles() {
           count={positions.length / 3}
           array={positions}
           itemSize={3}
+          args={[positions,3]}
         />
       </bufferGeometry>
       <pointsMaterial
